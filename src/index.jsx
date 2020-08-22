80287990f58e0c98aed4a54cd1513d76617c3a78
Joao Victor Ramires Guimaraes Brito <joaoramires.engsoft@gmail.com>
2020-08-22 10:14:05 -0300

    Chore: Add history dependency and fix BrowserRouter error

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 import logger from 'redux-logger';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { createHistory as history } from 'history';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import { reducer as formReducer } from 'redux-form';
 
 import CarsIndex from './containers/cars_index';
@@ -14,6 +14,8 @@ import CarsNew from './containers/cars_new';
 import '../assets/stylesheets/application.scss';
 
 import carsReducer from './reducers/cars_reducer.js';
+
+const history = createBrowserHistory();
 
 // State and reducers
 const garageName = prompt("What is your garage?") || `garage${Math.floor(10 + (Math.random() * 90))}`;
